83563b2959c962dbdf6fafa4b28e85021d5dc696
MehrabHussain47 <mehhussumon47@gmail.com>
2025-06-10 23:42:59 +0600

    Logic changes

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,13 +1,13 @@
-# CPU uses one choice for whole program 
+# Now CPU uses many choices for whole program 
 import random
 
 choices = ["Rock", "Paper", "Scissors"] # 3 choices nilam 
-computer = random.choice(choices)   # Choices theke randomly choose korba
 player = False  # player loop er jonno nilam
 cpu_score = 0   # Initially cpu and player er score zero
 player_score = 0
 
 while True:
+    computer = random.choice(choices)   # Choices theke randomly choose korba
     player = input("Rock, Paper or Scissors ? ").capitalize()
 
     # Conditions of the game
